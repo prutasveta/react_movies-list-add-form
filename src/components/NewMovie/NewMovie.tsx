13cceb949bd33db0fe60f58e771621f791cfca87
prutasveta <svetapruta@gmail.com>
2025-02-11 15:21:22 +0100

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,155 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState('');
+  const [isTitleValid, setIsTitleValid] = useState(false);
+
+  const [description, setDescription] = useState('');
+
+  const [imgUrl, setImgUrl] = useState('');
+  const [isImgUrlValid, setIsImgUrlValid] = useState(false);
+
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [isImdbUrlValid, setIsImdbUrlValid] = useState(false);
+
+  const [imdbId, setImdbId] = useState('');
+  const [isImdbIdValid, setIsImdbIdValid] = useState(false);
+
+  const handleChangeTitle = (text: string) => {
+    const newTitle = text.trim();
+
+    setTitle(newTitle);
+
+    setIsTitleValid(!!newTitle);
+  };
+
+  const handleChangeImgUrl = (text: string) => {
+    const newImgUrl = text.trim();
+
+    setImgUrl(newImgUrl);
+
+    setIsImgUrlValid(!!newImgUrl);
+  };
+
+  const handleChangeImbdUrl = (text: string) => {
+    const newImdbUrl = text.trim();
+
+    setImdbUrl(newImdbUrl);
+
+    setIsImdbUrlValid(!!newImdbUrl);
+  };
+
+  const handleChangeImbdId = (text: string) => {
+    const newImdbId = text.trim();
+
+    setImdbId(newImdbId);
+
+    setIsImdbIdValid(!!newImdbId);
+  };
+
+  const reset = () => {
+    // if (!title) {
+    //   return;
+    // }
+
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+
+    setIsTitleValid(false);
+    setIsImgUrlValid(false);
+    setIsImdbUrlValid(false);
+    setIsImdbIdValid(false);
+
+    setCount(count + 1);
+  };
+
+  const disabled =
+    !isTitleValid || !isImgUrlValid || !isImdbUrlValid || !isImdbIdValid;
+
+  const handleClick = () => {
+    // if (disabled) {
+    //   return;
+    // }
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    reset();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleClick}
+      onReset={reset}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={text => {
+          handleChangeTitle(text);
+        }}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={text => {
+          setDescription(text);
+        }}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={text => {
+          handleChangeImgUrl(text);
+        }}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={text => {
+          handleChangeImbdUrl(text);
+        }}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handleChangeImbdId}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +157,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={disabled}
           >
             Add
           </button>
